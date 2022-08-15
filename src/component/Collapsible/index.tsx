76eb24d9d187c45adf95dd334d695eff7ea363ea
Irin <iraantonova1998@list.ru>
2022-08-15 14:30:11 +0300

    component collapse

---
+++ src/component/Collapsible/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styles from './index.module.scss';
+
+interface IProps {
+  open?: boolean;
+  title: any;
+}
+
+const Collapsible: React.FC<IProps> = ({ open, children, title }) => {
+  const [isOpen, setIsOpen] = useState(open);
+
+  const handleFilterOpening = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.card_block}>
+          <h6 className={styles.h6}>
+            <div className={styles.wrapper_title}>{title}</div>
+          </h6>
+          <button
+            type="button"
+            className={styles.btn}
+            onClick={handleFilterOpening}
+          >
+            {isOpen ? (
+              <div className={styles.arrow_up} />
+            ) : (
+              <div className={styles.arrow_down} />
+            )}
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.card_children}>
+        {isOpen && <div className={styles.children}>{children}</div>}
+      </div>
+    </>
+  );
+};
+
+export default Collapsible;
--- src/component/PlanLimits/ItemFeatures/index.tsx
+++ src/component/PlanLimits/ItemFeatures/index.tsx
@@ -1,43 +1,54 @@
 import React from 'react';
+import Collapsible from 'component/Collapsible';
 import Star from '../../../icons/star.svg';
-import Close from '../../../icons/close.svg';
 import styles from './index.module.scss';
 
 const ItemFeatures = () => {
   return (
     <article>
-      <div className={styles.wrapper_features}>
-        <p className={styles.p_features}>Explore new features</p>
-        <img src={Star} alt="star.svg" />
-      </div>
-      <div className={styles.block}>
-        <div className={styles.block_header}>
-          <p className={styles.p_into_block_header}>Invoicing Premium</p>
-          <img src={Close} alt="close.svg" className={styles.close} />
-        </div>
-        <div className={styles.info}>
-          <ul className={styles.ul}>
-            <li>
-              <p>Unlimited outgoing e-invoices</p>
-            </li>
-            <li>
-              <p>Unlimited connected banks</p>
-            </li>
-            <li>
-              <p>10 users</p>
-            </li>
-            <li>
-              <p>Expense management</p>
-            </li>
-          </ul>
+      <Collapsible
+        open
+        title={
+          <>
+            <p>Explore new features</p>
+            <img src={Star} alt="star.svg" />
+          </>
+        }
+      >
+        <div className={styles.block}>
+          <div className={styles.block_header}>
+            <p className={styles.p_into_block_header}>Invoicing Premium</p>
+          </div>
+          <div className={styles.info}>
+            <ul className={styles.ul}>
+              <li>
+                <p>Unlimited outgoing e-invoices</p>
+              </li>
+              <li>
+                <p>Unlimited connected banks</p>
+              </li>
+              <li>
+                <p>10 users</p>
+              </li>
+              <li>
+                <p>Expense management</p>
+              </li>
+            </ul>
 
-          <div className={styles.price}>
-             <p>€20.99 <span>/mo</span></p>
-             <p className={styles.p_into_price}>billed annually or 24.99 € billed monthly</p>
-             <button type='button' className={styles.btn_upgrade}>Upgrade</button>
+            <div className={styles.price}>
+              <p>
+                €20.99 <span>/mo</span>
+              </p>
+              <p className={styles.p_into_price}>
+                billed annually or 24.99 € billed monthly
+              </p>
+              <button type="button" className={styles.btn_upgrade}>
+                Upgrade
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </Collapsible>
     </article>
   );
 };
